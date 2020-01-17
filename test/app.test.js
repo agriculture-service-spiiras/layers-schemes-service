72132171ba9b4dc11b75712e3746ac3ae35e8b95
Evgeny Karasev <gnomskg@gmail.com>
2020-01-17 17:41:17 +0300

    fix: Some project fix
    Disable show default error page, fix config for prettier

--- test/app.test.js
+++ test/app.test.js
@@ -24,22 +24,6 @@ describe('Feathers application tests (with jest)', () => {
   });
 
   describe('404', () => {
-    it('shows a 404 HTML page', async () => {
-      expect.assertions(2);
-      try {
-        await axios.get(getUrl('path/to/nowhere'), {
-          headers: {
-            Accept: 'text/html',
-          },
-        });
-      } catch (error) {
-        const { response } = error;
-
-        expect(response.status).toBe(404);
-        expect(response.data.indexOf('<html>')).not.toBe(-1);
-      }
-    });
-
     it('shows a 404 JSON error without stack trace', async () => {
       expect.assertions(4);
 
